aa66fbba1c4aa2a185ba1b0725a47b1acc1efd98
eisuelenfeijo <166126887+eisuelenfeijo@users.noreply.github.com>
2024-05-23 08:08:20 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,109 @@
+
+
+const nome = document.getElementById("cc");
+
+nome.onclick = ( ) => {
+	const variavel = nome.getAttribute("src");
+
+	if (variavel==="imagens/chinchila-comum.png"){
+		nome.setAttribute("src","imagens/chinchila-comum2.webp");
+	} else {
+		nome.setAttribute("src","imagens/chinchila-comum.png");
+	}
+};
+
+const nome2 = document.getElementById("ac");
+
+nome2.onclick = ( ) => {
+	const variavel = nome2.getAttribute("src");
+
+	if (variavel==="imagens/colibri-abelha-cubano.png"){
+		nome2.setAttribute("src","imagens/colibri-abelha-cubano2.jpg");
+	} else {
+		nome2.setAttribute("src","imagens/colibri-abelha-cubano.png");
+	}
+};
+
+
+const nome4 = document.getElementById("pkl");
+
+nome4.onclick = ( ) => {
+	const variavel = nome4.getAttribute("src");
+	if (variavel==="imagens/pika-de-lli.png"){
+		nome4.setAttribute("src","imagens/pika-de-lli2.jpg");
+	} else {
+		nome4.setAttribute("src","imagens/pika-de-lli.png");
+	}
+};
+
+const nome5 = document.getElementById("ca");
+
+nome5.onclick = ( ) => {
+	const variavel = nome5.getAttribute("src");
+
+	if (variavel==="imagens/castor-americano.png"){
+		nome5.setAttribute("src","imagens/castor-americano2.jpg");
+	} else {
+		nome5.setAttribute("src","imagens/castor-americano.png");
+	}
+};
+
+const nome6 = document.getElementById("leo");
+
+nome6.onclick = ( ) => {
+	const variavel = nome6.getAttribute("src");
+
+	if (variavel==="imagens/leopardo-das-neves.png"){
+		nome6.setAttribute("src","imagens/leopardo-das-neves2.jpg");
+	} else {
+		nome6.setAttribute("src","imagens/leopardo-das-neves.png");
+	}
+};
+
+const nome7 = document.getElementById("fc");
+
+nome7.onclick = ( ) => {
+	const variavel = nome7.getAttribute("src");
+
+	if (variavel==="imagens/foca-monge-do-mediterrâneo.png"){
+		nome7.setAttribute("src","imagens/foca-monge-do-mediterrâneo2.jpg");
+	} else {
+		nome7.setAttribute("src","imagens/foca-monge-do-mediterrâneo.png");
+	}
+};
+
+const nome8 = document.getElementById("dn");
+
+nome8.onclick = ( ) => {
+	const variavel = nome8.getAttribute("src");
+
+	if (variavel==="imagens/doninha-de-patas-pretas.png"){
+		nome8.setAttribute("src","imagens/doninha-de-patas-preta2.jpg");
+	} else {
+		nome8.setAttribute("src","imagens/doninha-de-patas-pretas.png");
+	}
+};
+
+const nome9 = document.getElementById("es");
+
+nome9.onclick = ( ) => {
+	const variavel = nome9.getAttribute("src");
+
+	if (variavel==="imagens/esquilo-vermelho.png"){
+		nome9.setAttribute("src","imagens/esquilo-vermelho2.jpg");
+	} else {
+		nome9.setAttribute("src","imagens/esquilo-vermelho.png");
+	}
+};
+
+const nome10 = document.getElementById("coa");
+
+nome10.onclick = ( ) => {
+	const variavel = nome10.getAttribute("src");
+
+	if (variavel==="imagens/coelho-angora.png"){
+		nome10.setAttribute("src","imagens/coelho-angora2.jpg");
+	} else {
+		nome10.setAttribute("src","imagens/coelho-angora.png");
+	}
+};
